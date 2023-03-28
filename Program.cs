6e7fb8beaa50eb99a6e7d5adee6b62e4b7e75d8d
KidChoque <lucassmachado1998@gmail.com>
2023-03-28 10:56:12 -0300

    Feito a introducao ao back

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿//string name = "Lucas";
+//Console.WriteLine(name);
+
+//int idade = 17;
+//idade= 30;
+
+
+
+//Console.WriteLine("A idade do " + name + " é " + idade + " anos");
+
+//atribuicao
+
+// int x = 15;
+
+// //operadores aritiméticos
+// //soma
+// Console.WriteLine(x+7);
+// Console.WriteLine(4+15);
+// Console.WriteLine(5+"4");
+
+
+
+// //subtração
+// Console.WriteLine(x-9);
+// Console.WriteLine(13+2);
+
+
+// //multiplicação
+// Console.WriteLine(x*2);
+// Console.WriteLine(3*2);
+
+
+
+// //divisão
+// Console.WriteLine(x/3);
+
+// //modular 
+// Console.WriteLine(5%2);
+
+
+//igual a
+Console.WriteLine($"Text");
+
+
+//maior igual a 
+
+//menor igual a
+
+//maior que
+
+//menor que
+
+//diferente de
+
+
+//operadores lógicos
+// && : e
+// || : ou
+// ! : não
+
+Console.WriteLine(5==5 & 8==8);
+
+
+
+//crie um programa para calcular o IMC de uma peassoa 
+//entrada:nome,idade,peso,altura
+//processamento: peso dividido por altura ao quadrado
+
+
+//entrada
+string name = "Lucas";
+
+int idade = 17;
+
+float peso = 72;
+
+float altura = 1.69f;
+
+
+//processamento
+
+float imc = peso / (altura*altura);
+
+
+//saída 
+Console.WriteLine("O IMC do " + name +  " é " + Math.Round(imc,2 ));
+
+
+
