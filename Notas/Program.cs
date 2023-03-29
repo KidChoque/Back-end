546acd68e412720e418ddbad92e799170b8d31e8
KidChoque <lucassmachado1998@gmail.com>
2023-03-29 10:58:48 -0300

    Feito o projeto notas

---
+++ Notas/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"Digite nota 1: ");
+
+float Nota1 = float.Parse(Console.ReadLine()
+);
+Console.WriteLine($"Digite nota 2: ");
+
+float Nota2 = float.Parse(Console.ReadLine()
+);
+Console.WriteLine($"Digite nota 3: ");
+
+float Nota3 = float.Parse(Console.ReadLine()
+);
+Console.WriteLine($"Digite nota 4: ");
+
+float Nota4 = float.Parse(Console.ReadLine()
+);
+Console.WriteLine($"Digite nota 5: ");
+
+float Nota5 = float.Parse(Console.ReadLine()
+);
+
+
+
+float média = (Nota1 + Nota2 + Nota3 + Nota4 + Nota5) / 5;
+
+Console.WriteLine($"Sua média é de {média}" );
+
